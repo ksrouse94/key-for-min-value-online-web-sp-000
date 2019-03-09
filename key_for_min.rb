b58a83feb094471439e803ddeb45425219103bc3
Kimberly Rouse <ksrouse94@gmail.com>
2019-03-09 03:11:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < lowest_value
       lowest_value = value
+      lowest_key = key
     end 
-    return key
+    return lowest_key
   end
   return nil
 end
